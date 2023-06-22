033e11d8327f09cc2705182db90479b0c5ef57c3
Kaptard <kaptaaard@gmail.com>
2023-06-22 17:07:22 +0200

    hotfix: Remove redundant domain events module

--- services/friends/src/infrastructure/InfrastructureModule.ts
+++ services/friends/src/infrastructure/InfrastructureModule.ts
@@ -1,20 +1,10 @@
 import { DynamicModule, Global, Module } from '@nestjs/common'
 import { defaultPinoHttpOptions } from '@ps2gg/nx/nest-app'
-import { DomainEventsModule } from '@ps2gg/nx/nest-domain-events'
 import { LoggerModule } from 'nestjs-pino'
-import { existsSync, readFileSync } from 'fs'
 import { ApplicationModule } from '../application/ApplicationModule'
 import { environment } from '../environment'
 import { HttpControllerModule } from './Controller/HttpControllerModule'
 import { TypeOrmModule } from './TypeOrm/TypeOrmModule'
-
-/**
- * process.env.NODE_ENV is forcibly replaced with development in the build process due to
- * the optimization flag being turned off in project.json. It should also stay off to avoid
- * misnaming of entities through minimization.
- * So instead, we check for the production state by the presence of the database secrets.
- */
-const prod = existsSync('/run/secrets/friends-db-pass')
 
 @Global()
 @Module({
@@ -23,9 +13,6 @@ const prod = existsSync('/run/secrets/friends-db-pass')
       pinoHttp: {
         ...defaultPinoHttpOptions(environment.env !== 'production'),
       },
-    }),
-    DomainEventsModule.forRoot({
-      eventStreamDsn: prod ? readFileSync('/run/secrets/notifications_mq_dsn', 'utf-8') : environment.eventStreamDsn,
     }),
 
     // infra
