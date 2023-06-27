6909a58767730b2d8d2ff0042acc6324fd6010e1
Kaptard <kaptaaard@gmail.com>
2023-06-27 22:30:25 +0200

    feat: Add ulid validation

--- libs/common/util/src/lib/validation.ts
+++ libs/common/util/src/lib/validation.ts
@@ -10,3 +10,9 @@ export function validateCharacterId(value: string): void {
     throw new Error('Expected characterId to be 19 characters long')
   }
 }
+
+export function validateULID(value: string): void {
+  if (!/^[0-9A-Z]{26}$/.test(value)) {
+    throw new Error('Expected ulid to be a 26 character alphanumeric string')
+  }
+}
