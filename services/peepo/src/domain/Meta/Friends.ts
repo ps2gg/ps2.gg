0ee2a725745fd389ac442d33351602e01504516b
Admutin <admutin@gmail.com>
2023-06-29 23:25:40 +0200

    feat: Name Parameter optional

--- services/peepo/src/domain/Meta/Friends.ts
+++ services/peepo/src/domain/Meta/Friends.ts
@@ -4,13 +4,13 @@ export const Friends: CommandConfig = {
   name: 'friends',
   description: 'Shows a list of all your friends',
   options: [
-    // {
-    //   type: 'string',
-    //   name: 'name',
-    //   description: 'your ingame name, will be removed as soon as verification is stable',
-    //   required: true,
-    //   autocomplete: true,
-    // },
+    {
+      type: 'string',
+      name: 'name',
+      description: 'your ingame name, will be removed as soon as verification is stable',
+      required: false,
+      autocomplete: true,
+    },
   ],
 }
 
