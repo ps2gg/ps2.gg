a52521222d53206e0f329e95577c6d718e84e580
Kilian <kaptaaard@gmail.com>
2023-07-05 15:19:43 +0200

    fix: Allow multiple dev instances to respond to commands (#86)

--- services/peepo/src/infrastructure/SlashCommand/Sesh.ts
+++ services/peepo/src/infrastructure/SlashCommand/Sesh.ts
@@ -1,5 +1,6 @@
 import { getPlayerAutocomplete } from '@ps2gg/alts/ws'
 import { Command, Main, Autocomplete, AutocompleteResponse, linkedUser, CommandResponse } from '@ps2gg/discord/command'
+import { defer, reply } from '@ps2gg/discord/util'
 import { User } from '@ps2gg/users/types'
 import { CommandInteraction } from 'discord.js'
 import { validateVerification } from '../../application/Command/ValidateVerification'
@@ -11,14 +12,14 @@ import { Sesh, SeshOptions } from '../../domain/Meta/Sesh'
 export class SeshCommand {
   @Main(Sesh)
   async sesh(options: SeshOptions, @linkedUser user: User, interaction: CommandInteraction): Promise<CommandResponse> {
-    await interaction.deferReply({ ephemeral: true })
+    await defer(interaction, true)
     const { player: name } = options
 
     if (!name && !user.characterIds?.length) return { interactionContext: [], embeds: [new VerifyHintEmbed()], ephemeral: true }
     if (name) await validateVerification(name, user, interaction)
 
     const friends = await getAltWideFriends(user, name)
-    await interaction.editReply({ embeds: [friends] })
+    await reply(interaction, { embeds: [friends] })
     const friendsAllAlts = await getAltWideFriends(user, name, true)
     return {
       interactionContext: [],
