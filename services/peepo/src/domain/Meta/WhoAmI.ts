a4d2d2966afe07d1ee1bf1578b91b9f0f686a5b8
Admutin <admutin@gmail.com>
2023-07-05 19:37:29 +0200

    Feat(peepo): Added whoami Command (#87)
    * chore(discord): Updated Discord submodule
    
    * feat(peepo): Created WhoAmI Command
    
    * fix(peepo): Removed OutiftTag from Friends and Whoami cmd if not set

---
+++ services/peepo/src/domain/Meta/WhoAmI.ts
@@ -0,0 +1,7 @@
+import { CommandConfig } from '@ps2gg/discord/command'
+
+export const WhoAmI: CommandConfig = {
+  name: 'whoami',
+  description: 'Returns all Characters that you verified.',
+  options: [],
+}
