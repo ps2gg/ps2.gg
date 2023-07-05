3a05b0c76cedc6f46588b17fe9d6c76616185a6e
Admutin <admutin@gmail.com>
2023-07-05 19:35:28 +0200

    fix(peepo): Used the right function to get players

--- services/peepo/src/infrastructure/SlashCommand/WhoAmI.ts
+++ services/peepo/src/infrastructure/SlashCommand/WhoAmI.ts
@@ -1,8 +1,7 @@
 import { Command, Main, CommandResponse, linkedUser } from '@ps2gg/discord/command'
 import { DiscordCommand } from '@ps2gg/discord/types'
-import { PlayerClient } from '@ps2gg/players/client'
 import { User } from '@ps2gg/users/types'
-import { FriendsEmbed } from '../../domain/Embed/FriendsEmbed'
+import { getPlayers } from '../../application/Query/GetPlayers'
 import { VerifiedCharactersEmbed } from '../../domain/Embed/VerifiedCharactersEmbed'
 import { WhoAmI } from '../../domain/Meta/WhoAmI'
 
@@ -10,9 +9,7 @@ import { WhoAmI } from '../../domain/Meta/WhoAmI'
 export class WhoAmICommand extends DiscordCommand {
   @Main(WhoAmI)
   async whoAmI(options: null, @linkedUser user: User): Promise<CommandResponse> {
-    const players = new PlayerClient()
-
-    const friends = user.characterIds.length ? await players.findMany(user.characterIds) : []
+    const friends = user.characterIds.length ? await getPlayers(user.characterIds) : []
     const embed = new VerifiedCharactersEmbed(friends)
     return {
       interactionContext: [],
