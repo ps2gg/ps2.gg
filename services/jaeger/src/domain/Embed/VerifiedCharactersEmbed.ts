78e6983517cd828e6414a997a3b826c96a6b689f
Kaptard <kaptaaard@gmail.com>
2023-07-06 15:04:06 +0200

    feat: Add whoami command stump

---
+++ services/jaeger/src/domain/Embed/VerifiedCharactersEmbed.ts
@@ -0,0 +1,20 @@
+import { factions } from '@ps2gg/common/constants'
+import { code } from '@ps2gg/discord/util'
+import { Player } from '@ps2gg/players/types'
+import { APIEmbed } from 'discord.js'
+
+function printFriends(players: Player[], faction: string): string {
+  players = players.filter((f) => factions[f.factionId] == faction)
+  return players.length ? `${faction}\n ${code(players.map((friend) => `${friend.outfitTag ? `[${friend.outfitTag}]` : ''}  ${friend.name}`).join('\n'))} \n` : ''
+}
+
+export class VerifiedCharactersEmbed implements APIEmbed {
+  title = 'Verified Characters'
+  description: string
+
+  constructor(friends: Player[]) {
+    this.description = friends.length
+      ? `${printFriends(friends, 'NC')} ${printFriends(friends, 'TR')} ${printFriends(friends, 'VS')}  ${printFriends(friends, 'NS')}`
+      : code('No Characters verified.')
+  }
+}
