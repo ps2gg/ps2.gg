70dc7fa69cd14066175262ffc22ae7a839b86e92
Kaptard <kaptaaard@gmail.com>
2023-07-06 15:51:21 +0200

    style: Use new verified character embed

--- services/jaeger/src/domain/Embed/VerifiedCharactersEmbed.ts
+++ services/jaeger/src/domain/Embed/VerifiedCharactersEmbed.ts
@@ -1,20 +1,33 @@
 import { factions } from '@ps2gg/common/constants'
+import { emojis } from '@ps2gg/discord/constants'
 import { code } from '@ps2gg/discord/util'
 import { Player } from '@ps2gg/players/types'
-import { APIEmbed } from 'discord.js'
-
-function printFriends(players: Player[], faction: string): string {
-  players = players.filter((f) => factions[f.factionId] == faction)
-  return players.length ? `${faction}\n ${code(players.map((friend) => `${friend.outfitTag ? `[${friend.outfitTag}]` : ''}  ${friend.name}`).join('\n'))} \n` : ''
-}
+import { APIEmbed, APIEmbedField } from 'discord.js'
 
 export class VerifiedCharactersEmbed implements APIEmbed {
-  title = 'Verified Characters'
-  description: string
+  description = '## Everyone we know you as'
+  footer = { text: 'To link more characters, use /verify' }
+  fields: APIEmbedField[] = []
 
   constructor(friends: Player[]) {
-    this.description = friends.length
-      ? `${printFriends(friends, 'NC')} ${printFriends(friends, 'TR')} ${printFriends(friends, 'VS')}  ${printFriends(friends, 'NS')}`
-      : code('No Characters verified.')
+    if (!friends.length) this.description += code('No characters linked')
+
+    const nc = getFriendsByFaction(friends, 'NC')
+    const tr = getFriendsByFaction(friends, 'TR')
+    const vs = getFriendsByFaction(friends, 'VS')
+
+    if (nc) this.fields.push(nc)
+    if (tr) this.fields.push(tr)
+    if (vs) this.fields.push(vs)
   }
+}
+
+function getFriendsByFaction(players: Player[], faction: string): APIEmbedField | null {
+  players = players.filter((f) => factions[f.factionId] == faction)
+  return players.length
+    ? {
+        name: `${emojis[faction.toLowerCase()]} ${faction}`,
+        value: `${code(players.map((friend) => `${friend.outfitTag ? `[${friend.outfitTag}] ` : ''}${friend.name}`).join('\n'))}`,
+      }
+    : null
 }
