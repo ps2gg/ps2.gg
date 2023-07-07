218ca12b64c9872ac5d896b97555c59541e5d96a
Kaptard <kaptaaard@gmail.com>
2023-07-07 06:24:09 +0200

    refactor: Split up constants into individual files

---
+++ libs/common/constants/src/lib/alts.ts
@@ -0,0 +1,12 @@
+export const ps2gg = {
+  altsUrl: 'http://alts:3003',
+}
+
+export const roleThresholds = {
+  pilot: {
+    esf: {
+      playTimePercent: 0.2,
+      kills: 3000,
+    },
+  },
+}
