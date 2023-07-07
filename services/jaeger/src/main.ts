0b82725af1e894a54800799f94ad5ca684799d5e
Kaptard <kaptaaard@gmail.com>
2023-07-07 11:54:55 +0200

    feat(jaeger): Add whoami command

--- services/jaeger/src/main.ts
+++ services/jaeger/src/main.ts
@@ -1,11 +1,12 @@
 import { setupDiscordClient } from '@ps2gg/discord/client'
 import { readFileSync } from 'fs'
 import { VerifyCommand } from './infrastructure/SlashCommand/Verify'
+import { WhoAmICommand } from './infrastructure/SlashCommand/WhoAmI'
 
 setupDiscordClient({
   id: '1118852327898624081',
   token: readFileSync('/run/secrets/jaeger-token', 'utf-8'),
-  commands: [new VerifyCommand()],
+  commands: [new VerifyCommand(), new WhoAmICommand()],
   restrictions: {
     development: {
       channels: ['1107543568807116920'],
