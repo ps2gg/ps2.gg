3ffbf99cf3e574d804abe35b33dc6118b35e212d
Kaptard <kaptaaard@gmail.com>
2023-07-08 13:00:29 +0200

    fix: Add empty options to command metas where they're missing

--- services/peepo/src/domain/Meta/SetupEarlyAccess.ts
+++ services/peepo/src/domain/Meta/SetupEarlyAccess.ts
@@ -3,4 +3,5 @@ import { CommandConfig } from '@ps2gg/discord/command'
 export const SetupEarlyAccess: CommandConfig = {
   name: 'early-access',
   description: 'Makes early access channels available to subscribers',
+  options: [],
 }
