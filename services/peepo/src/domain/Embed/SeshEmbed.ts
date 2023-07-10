edeb5000a28aba098822061bd720ab66c7d64351
Kaptard <kaptaaard@gmail.com>
2023-07-10 07:01:32 +0200

    patch/refactor: Add embed color to sesh embed, enforce single responsibilities

--- services/peepo/src/domain/Embed/SeshEmbed.ts
+++ services/peepo/src/domain/Embed/SeshEmbed.ts
@@ -1,3 +1,4 @@
+import { EmbedColors } from '@ps2gg/discord/constants'
 import { code } from '@ps2gg/discord/util'
 import { Player } from '@ps2gg/players/types'
 import { APIEmbed, APIEmbedField, APIEmbedFooter } from 'discord.js'
@@ -6,24 +7,33 @@ export class SeshEmbed implements APIEmbed {
   description: string
   fields?: APIEmbedField[]
   footer?: APIEmbedFooter
+  color?: EmbedColors
 
   constructor(friends: Player[], player: Player) {
-    this.description = `## The best place to be, at all times
+    this.description = this._getDescription(friends)
+    this.footer = player && player.isOnline ? this._getOnlineFooter(player) : this._getOfflineFooter()
+    if (player.isOnline) this.color = EmbedColors.Success
+  }
+
+  private _getDescription(friends: Player[]) {
+    return `## The best place to be, at all times
     Never waste your time with bad fights again
 ### Best Fights
     Coming soon:tm:\n### Who's playing?
     ${code(friends.length ? friends.map((friend) => friend.name).join('\n') : 'No frens online')}`
+  }
 
-    if (player && player.isOnline) {
-      this.footer = {
-        icon_url: 'https://cdn.discordapp.com/emojis/717334812083355658.webp?size=240&quality=lossless',
-        text: `Playing as ${player.name}`,
-      }
-    } else {
-      this.footer = {
-        icon_url: 'https://cdn.discordapp.com/emojis/717334809621430352.webp?size=240&quality=lossless',
-        text: "We don't see you online",
-      }
+  private _getOnlineFooter(player) {
+    return {
+      icon_url: 'https://cdn.discordapp.com/emojis/717334812083355658.webp?size=240&quality=lossless',
+      text: `Playing as ${player.name}`,
+    }
+  }
+
+  private _getOfflineFooter() {
+    return {
+      icon_url: 'https://cdn.discordapp.com/emojis/717334809621430352.webp?size=240&quality=lossless',
+      text: "We don't see you online",
     }
   }
 }
