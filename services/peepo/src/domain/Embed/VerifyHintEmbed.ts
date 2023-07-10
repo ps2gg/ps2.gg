fd024662ec946d72cfa72b5513d2ae5b2fcc09ea
Kaptard <kaptaaard@gmail.com>
2023-07-11 00:11:32 +0200

    patch: Use latest verify command id in hint

--- services/peepo/src/domain/Embed/VerifyHintEmbed.ts
+++ services/peepo/src/domain/Embed/VerifyHintEmbed.ts
@@ -4,6 +4,6 @@ export class VerifyHintEmbed implements APIEmbed {
   description: string
 
   constructor() {
-    this.description = '### Verification required\n' + `Please use </verify:1124474254814089286> to link an ingame character.`
+    this.description = '### Verification required\n' + `Please use </verify:1126581814312579098> to link an ingame character.`
   }
 }
